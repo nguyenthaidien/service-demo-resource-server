a7b50f52d0c4bc2bc5c08e725dbcd2c679d7b24d
nguyenthaidien <45583973+nguyenthaidien@users.noreply.github.com>
2025-05-20 10:09:37 +0700

    Bo sung code

---
+++ src/main/java/com/amrut/prabhu/product/ProductServiceApplication.java
@@ -0,0 +1,20 @@
+package com.amrut.prabhu.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/* import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile; */
+
+/* @Configuration(proxyBeanMethods = false)
+@Profile("prod") */
+@SpringBootApplication
+@EnableEurekaClient
+public class ProductServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
